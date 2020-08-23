a4f9cc5307998fd3a087cd59c4d08bf22abaf42d
seungmi <52492579+seungmi93@users.noreply.github.com>
2020-08-24 08:06:49 +0900

    my num add

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, jsonify, request
-import requests
-from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -31,10 +29,39 @@ def get_store():
     result = list(db.store.find({}, {'_id': False}))
     return jsonify({'result': 'success', 'store': result})
 
+
 @app.route('/my-num', methods=['GET'])
 def get_my_num():
-    return jsonify({'result': 'success', 'my_result': result})
+    number_list = [
+        request.args.get('firstRow01'),
+        request.args.get('firstRow02'),
+        request.args.get('firstRow03'),
+        request.args.get('firstRow04'),
+        request.args.get('firstRow05'),
+        request.args.get('firstRow06')
+    ]
 
+    result = list(db.win_num.find({}, {'_id': False}).sort('drwNo', -1).limit(1))
+
+    result_list = [
+        result[0]['drwtNo1'],
+        result[0]['drwtNo2'],
+        result[0]['drwtNo3'],
+        result[0]['drwtNo4'],
+        result[0]['drwtNo5'],
+        result[0]['drwtNo6']
+    ]
+
+    print('db result : ', result_list)
+
+    correct_list = []
+    for number in number_list:
+        if int(number) in result_list:
+            correct_list.append(number)
+        else:
+            correct_list.append(-1)
+
+    return jsonify({'result': 'success', 'my_num': correct_list})
 
 
 if __name__ == '__main__':
